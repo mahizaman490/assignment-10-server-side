2a6ae7b6bd8b3424cf1b6c0f95b669410453d384
mahizaman490 <mahizaman490@gmail.com>
2023-10-21 12:53:54 +0600

    updated the package.json file

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ async function run() {
     const id = req.params.id
     const query= {_id:new ObjectId(id)}
     const result = await cartProductsCollection.deleteOne(query) 
-    res.send.(result)
+    res.send(result)
     
   })
 
